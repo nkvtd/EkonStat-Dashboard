0647f5be670f3e1cd8f926516d9126be00920bb0
Tome <gjorgievtome@gmail.com>
2026-05-06 01:54:21 +0200

    fix(config): Add prerender to configuration

--- pages/+config.ts
+++ pages/+config.ts
@@ -2,6 +2,7 @@ import type { Config } from "vike/types";
 import vikeVue from "vike-vue/config";
 
 const config: Config = {
+    prerender: true,
     ssr: false,
     title: "EkonStat-Dashboard",
     description: "Tool for visualising economic statistics",
